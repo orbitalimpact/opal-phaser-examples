ed7a184169d270e7221eccd2aa5f4ed3fae74fbb
Andre Anastacio <andreluisanastacio@gmail.com>
2014-12-26 20:27:51 -0200

    Fix 'Load an Animation' example

--- examples/load_an_animation/load_an_animation.rb
+++ examples/load_an_animation/load_an_animation.rb
@@ -4,18 +4,21 @@ require 'opal-phaser'
 class LoadAnAnimation
   def initialize
     preload = proc do
-      @game.native.load.atlasJSONHash('bot',
-                                      'assets/sprites/running_bot.png',
-                                      'assets/sprites/running_bot.json')
+      # On original examples uses atlasJSONHash
+      # but throw a exception:
+      # TypeError: undefined is not an object (evaluating 'g[i].frame.x')
+      @game.load.atlasJSONArray('bot',
+                                'assets/sprites/running_bot.png',
+                                'assets/sprites/running_bot.json')
     end
 
     create = proc do
-      bot = @game.native.add.sprite(200, 200, 'bot')
-      bot.native.animations.add('run')
-      bot.native.animations.play('run', 15, true)
+      bot = @game.add.sprite(200, 200, 'bot')
+      bot.animations.add('run')
+      bot.animations.play('run', 15, true)
     end
 
     state = `{ preload: preload, create: create }`
-    @game = Opal::Phaser::Game.new(800, 600, Opal::Phaser::AUTO, 'phaser-example', state)
+    @game = Opal::Phaser::Game.new(800, 600, Opal::Phaser::CANVAS, 'phaser-example', state)
   end
 end
