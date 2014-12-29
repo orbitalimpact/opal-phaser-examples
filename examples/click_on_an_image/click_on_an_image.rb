5774a1f1b56a014c9470ddce2c9827c3906dd48e
George D. Plymale II <georgedp@orbitalimpact.com>
2014-12-29 15:31:35 -0500

    replaced old opal-phaser code with new code

--- examples/click_on_an_image/click_on_an_image.rb
+++ examples/click_on_an_image/click_on_an_image.rb
@@ -2,28 +2,32 @@ require 'opal'
 require 'opal-phaser'
 
 class ClickOnAnImage
-  def initialize
-    @counter = 0
-
-    listener = proc do
-      @counter += 1
-      @text.text = "You clicked #{@counter} times"
+    def initialize
+        @text = nil
+        @counter = 0
+        
+        @phaser = Phaser::Game.new(800, 600) do |state|
+            
+            state.preload do |game|
+                game.load.image("einstein", "assets/pics/ra_einstein.png")
+            end
+            
+            listener = Proc.new do
+                @counter += 1
+                @text.text = "You clicked #{@counter} times!"
+            end
+            
+            state.create do |game|
+                @image = game.add.sprite(game.world.centerX, game.world.centerY, "einstein")
+                
+                @image.anchor.set(0.5)
+                
+                @image.inputEnabled = true
+                
+                @text = game.add.text(250, 16, '', { fill: "#ffffff" })
+                
+                @image.events.onInputDown.add(listener)
+            end
+        end
     end
-
-    preload = proc do
-      @game.load.image('einsten', 'assets/pics/ra_einstein.png')
-    end
-
-    create = proc do
-      image = @game.add.sprite(@game.world.centerX, @game.world.centerY, 'einsten')
-      image.anchor.set(0.5)
-      image.inputEnabled = true
-      @text = @game.add.text(250, 16, '', `{ fill: '#ffffff' }`)
-
-      image.events.onInputDown.add(listener.to_n, self)
-    end
-
-    state = `{ preload: preload, create: create }`
-    @game = Opal::Phaser::Game.new(800, 600, Opal::Phaser::CANVAS, 'phaser-example', state )
-  end
 end
