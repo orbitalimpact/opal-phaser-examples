d9daf78f01cc642e1383e8f0ad4df6d20a790ced
George D. Plymale II <georgedp@orbitalimpact.com>
2014-12-29 16:14:22 -0500

    moved listener and the variables to state.create

--- examples/click_on_an_image/click_on_an_image.rb
+++ examples/click_on_an_image/click_on_an_image.rb
@@ -3,21 +3,21 @@ require 'opal-phaser'
 
 class ClickOnAnImage
     def initialize
-        @text = nil
-        @counter = 0
-        
         @phaser = Phaser::Game.new(800, 600) do |state|
             
             state.preload do |game|
                 game.load.image("einstein", "assets/pics/ra_einstein.png")
             end
             
-            listener = Proc.new do
-                @counter += 1
-                @text.text = "You clicked #{@counter} times!"
-            end
-            
             state.create do |game|
+                @text = nil
+                @counter = 0
+                
+                listener = Proc.new do
+                    @counter += 1
+                    @text.text = "You clicked #{@counter} times!"
+                end
+                
                 @image = game.add.sprite(game.world.centerX, game.world.centerY, "einstein")
                 
                 @image.anchor.set(0.5)
