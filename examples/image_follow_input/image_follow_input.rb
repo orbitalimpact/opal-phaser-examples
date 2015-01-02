cec74c153d911f1e9e279f9bf54866df974fc53e
George D. Plymale II <georgedp@orbitalimpact.com>
2015-01-02 11:50:48 -0500

    added basic structure of the example

--- examples/image_follow_input/image_follow_input.rb
+++ examples/image_follow_input/image_follow_input.rb
@@ -2,5 +2,20 @@ require 'opal'
 require 'opal-phaser'
 
 class ImageFollowInput
+    def initialize
+        state = Phaser::State.new
+        state.preload do |game|
+            game.load.image("phaser", "assets/sprites/phaser.png")
+        end
+        
+        state.create do |game|
+            # we won't do anything... yet
+        end
+        
+        state.update do |game|
+            # same here
+        end
+        
+        @phaser = Phaser::Game.new(800, 600, Phaser::AUTO, '', state)
     end
 end
