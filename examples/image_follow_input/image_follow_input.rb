9d8933a01170e4127a562c6aaddc090723c36e32
George D. Plymale II <georgedp@orbitalimpact.com>
2015-01-02 11:50:37 -0500

    removed old opal-phaser code; some of it's incompatible now

--- examples/image_follow_input/image_follow_input.rb
+++ examples/image_follow_input/image_follow_input.rb
@@ -2,33 +2,5 @@ require 'opal'
 require 'opal-phaser'
 
 class ImageFollowInput
-  def initialize
-    preload = proc do
-      @game.load.image('phaser', 'assets/sprites/phaser.png')
     end
-
-    create = proc do
-      @game.physics.startSystem(Opal::Phaser::Physics::ARCADE)
-      @sprite = @game.add.sprite(@game.world.centerX, @game.world.centerY, 'phaser')
-      @sprite.anchor.set(0.5)
-      @game.physics.arcade.enable(@sprite)
-    end
-
-    update = proc do
-      distance_to_pointer = @game.physics.arcade.distanceToPointer(@sprite, @game.input.activePointer)
-
-      if distance_to_pointer > 8
-        @game.physics.arcade.moveToPointer(@sprite, 300)
-      else
-        @sprite.body.velocity.set(0)
-      end
-    end
-
-    render = proc do
-      @game.debug.inputInfo(32, 32)
-    end
-
-    state = `{ preload: preload, create: create, update: update, render: render }`
-    @game = Opal::Phaser::Game.new(800, 600, Opal::Phaser::AUTO, 'phaser-example', state )
-  end
 end
