a3d676801b76960b21603e4866b83455697ea5eb
André Rodrigues <andrerpbts@gmail.com>
2015-01-06 21:24:39 -0200

    Update click_on_an_image.rb

--- examples/click_on_an_image/click_on_an_image.rb
+++ examples/click_on_an_image/click_on_an_image.rb
@@ -29,6 +29,8 @@ module Entity
 end
 
 class ClickOnAnImage
+  attr_reader :einstein, :counter
+  
   def initialize
     @@einstein = Entity::Einstein.new
     @@counter = Counter.new
@@ -37,17 +39,17 @@ class ClickOnAnImage
   def run
     Phaser::Game.new(800, 600) do |state|
       state.preload do |game|
-        game.load.image(@@einstein.sprite_key, @@einstein.sprite_url)
+        game.load.image(einstein.sprite_key, einstein.sprite_url)
       end
 
       state.create do |game|
-        image = game.add.sprite(game.world.centerX, game.world.centerY, @@einstein.sprite_key)
+        image = game.add.sprite(game.world.centerX, game.world.centerY, einstein.sprite_key)
         image.anchor.set(0.5)
         text = game.add.text(250, 16, '', `{ fill: '#ffffff' }`)
 
         listener = proc {
-          @@counter.add
-          text.text = @@counter.to_s
+          counter.add
+          text.text = counter.to_s
         }
 
         image.inputEnabled = true
