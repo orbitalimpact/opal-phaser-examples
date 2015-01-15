9ce683b91d4063ff5d3e88e9d73bde228cb5ebe6
George D. Plymale II <georgedp@orbitalimpact.com>
2015-01-15 18:53:28 -0500

    created move an image example

--- examples/move_an_image/move_an_image.rb
+++ examples/move_an_image/move_an_image.rb
@@ -2,18 +2,18 @@ require 'opal'
 require 'opal-phaser'
 
 class MoveAnImage
-  def initialize
-    preload = proc do
-      @game.load.image('einsten', 'assets/pics/ra_einstein.png')
+    def initialize
+        state = Phaser::State.new
+        state.preload do |game|
+            game.load.image('einsten', 'assets/pics/ra_einstein.png')
+        end
+      
+        state.create do |game|
+            image = game.add.sprite(0, 0, 'einsten')
+            game.physics.enable(image, Phaser::Physics::ARCADE)
+            image.body.velocity.x = 150
+        end
+      
+        @phaser = Phaser::Game.new(800, 600, Phaser::AUTO, 'example', state)
     end
-
-    create = proc do
-      image = @game.add.sprite(0, 0, 'einsten')
-      @game.physics.enable(image, Opal::Phaser::Physics::ARCADE)
-      image.body.velocity.x = 150
-    end
-
-    state = `{ preload: preload, create: create }`
-    @game = Opal::Phaser::Game.new(800, 600, Opal::Phaser::CANVAS, 'phaser-example', state )
-  end
 end
