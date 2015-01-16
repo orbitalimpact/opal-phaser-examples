dc1555644d1c37bccfeb5f1986808a378495b28d
George D. Plymale II <georgedp@orbitalimpact.com>
2015-01-16 10:58:25 -0500

    added tween_an_image example

--- examples/tween_an_image/tween_an_image.rb
+++ examples/tween_an_image/tween_an_image.rb
@@ -2,19 +2,19 @@ require 'opal'
 require 'opal-phaser'
 
 class TweenAnImage
-  def initialize
-    preload = proc do
-      @game.load.image('einstein', 'assets/pics/ra_einstein.png')
+    def initialize
+        state = Phaser::State.new
+        state.preload do |game|
+            game.load.image('einstein', 'assets/pics/ra_einstein.png')
+        end
+        
+        state.create do |game|
+            sprite = game.add.sprite(-400, 0, 'einstein')
+            tween = game.add.tween(sprite)
+            tween.to( {x: 600}, 6000 )
+            tween.start
+        end
+        
+        @phaser = Phaser::Game.new(800, 600, Phaser::CANVAS, 'example', state)
     end
-
-    create = proc do
-      sprite = @game.add.sprite(-400, 0, 'einstein')
-      tween  = @game.add.tween(sprite)
-      tween.to(`{ x: 600 }`, 6000)
-      tween.start
-    end
-
-    state = `{ preload: preload, create: create }`
-    @game = Opal::Phaser::Game.new(800, 600, Opal::Phaser::CANVAS, 'phaser-example', state )
-  end
 end
