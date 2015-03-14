fe0f23fb0c96a9480984a9e2d53e508185c6bed7
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-14 13:07:56 -0400

    changed "Image Follow Input" for the new API

--- examples/basics/image_follow_input/config.ru
+++ examples/basics/image_follow_input/config.ru
@@ -3,7 +3,7 @@ Bundler.require
 
 opal = Opal::Server.new {|s|
   s.append_path '.'
-  s.append_path '../assets'
+  s.append_path '../../assets'
   s.main = 'image_follow_input'
 }
 
@@ -16,7 +16,7 @@ get '/' do
     <!doctype html>
     <html>
       <head>
-          <title>Image follow input</title>
+          <title>Image Follow Input</title>
           <script src="http://cdnjs.cloudflare.com/ajax/libs/phaser/2.2.1/phaser.min.js"></script>
           <script src="/assets/image_follow_input.js"></script>
       </head>
@@ -47,7 +47,7 @@ get '/' do
       <body>
         <script>
           window.onload = function() {
-            Opal.ImageFollowInput.$new()
+            Opal.Game.$new()
           }
         </script>
         <a href="https://github.com/orbitalimpact/opal-phaser-examples">
--- examples/basics/image_follow_input/image_follow_input.rb
+++ examples/basics/image_follow_input/image_follow_input.rb
@@ -1,38 +1,87 @@
 require 'opal'
 require 'opal-phaser'
 
-class ImageFollowInput
-    def initialize
-        sprite = nil
-        
-        state = Phaser::State.new
-        state.preload do |game|
-            game.load.image("phaser", "assets/sprites/phaser.png")
-        end
-        
-        state.create do |game|
-            game.physics.arcade.startSystem(Phaser::Physics::ARCADE)
-            
-            sprite = game.add.sprite(game.world.centerX, game.world.centerY, "phaser")
-            sprite.anchor.set(0.5)
-            
-            game.physics.arcade.enable(sprite)
-        end
-        
-        state.update do |game|
-            distance_from_cursor = game.physics.arcade.distanceToPointer(sprite, game.input.activePointer)
-            
-            if distance_from_cursor > 8
-                game.physics.arcade.moveToPointer(sprite, 300)
-            else
-                sprite.body.velocity.set(0)
-            end
-        end
-        
-        state.render do |game|
-            game.debug.inputInfo(32, 32)
-        end
-        
-        @phaser = Phaser::Game.new(800, 600, Phaser::AUTO, 'example', state)
+class Logo
+  def initialize(game)
+    @sprite_key = "phaser"
+    @sprite_url = "assets/sprites/phaser.png"
+    @game       = game
+  end
+  
+  def preload
+    @game.load.image(@sprite_key, @sprite_url)
+  end
+  
+  def create
+    @speed_to_pointer = 300
+    @debug_x_pos      = 32
+    @debug_y_pos      = 32
+    
+    @game.physics.start_system(Phaser::Physics::ARCADE)
+    
+    @sprite = @game.add.sprite(@game.world.x_center, @game.world.y_center, "phaser")
+    @sprite.anchor.set(0.5)
+    
+    @game.physics.arcade.enable(@sprite)
+  end
+  
+  def update
+    if @game.physics.arcade.distance_to_pointer(@sprite, @game.input.activePointer) > 8
+      @game.physics.arcade.move_to_pointer(@sprite, @speed_to_pointer)
+    else
+      @sprite.body.velocity.set(0)
     end
+  end
+  
+  def render
+    @game.debug.inputInfo(@debug_x_pos, @debug_y_pos)
+  end
+end
+
+class Game
+  def initialize
+    preload
+    create_game
+    update_game
+    render
+    
+    Phaser::Game.new(width: 800, height: 600, renderer: Phaser::AUTO, parent: "example", state: state, transparent: false, antialias: true, physics: nil)
+  end
+  
+  def preload
+    state.preload do |game|
+      initialize_entities(game)
+      entities_call :preload
+    end
+  end
+  
+  def create_game
+    state.create do
+      entities_call :create
+    end
+  end
+  
+  def update_game
+    state.update do
+      entities_call :update
+    end
+  end
+  
+  def render
+    state.render do
+      entities_call :render
+    end
+  end
+  
+  def state
+    @state ||= Phaser::State.new
+  end
+  
+  def initialize_entities(game)
+    @image = Logo.new(game)
+  end
+  
+  def entities_call(method)
+    @image.send(method)
+  end
 end
