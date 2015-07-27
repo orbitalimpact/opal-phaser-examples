3960783b285ce0857f834ba3326c2ec63def318c
George D. Plymale II <georgedp@orbitalimpact.com>
2015-07-27 12:28:52 -0400

    remove semicolons
    @gabrielrios must have not realized he was using all these semicolons ;)

--- examples/bitmap_data/alpha_mask/main.rb
+++ examples/bitmap_data/alpha_mask/main.rb
@@ -3,24 +3,24 @@ require 'opal-phaser'
 
 class AlphaMask < Phaser::State
   def preload
-    game.load.image('pic', 'assets/pics/questar.png');
-    game.load.image('mask', 'assets/pics/mask-test2.png');
+    game.load.image('pic', 'assets/pics/questar.png')
+    game.load.image('mask', 'assets/pics/mask-test2.png')
   end
 
   def create
     game.stage.background_color = 0x4d4d4d
 
     game.add.text(64, 10, 'Source image', { font: '16px Arial', fill: '#ffffff' })
-    game.add.image(64, 32, 'pic');
+    game.add.image(64, 32, 'pic')
 
     game.add.text(400, 10, 'Alpha mask', { font: '16px Arial', fill: '#ffffff' })
-    game.add.image(400, 32, 'mask');
+    game.add.image(400, 32, 'mask')
 
-    bmd = game.make.bitmap_data(320, 256);
+    bmd = game.make.bitmap_data(320, 256)
 
-    bmd.alpha_mask('pic', 'mask');
+    bmd.alpha_mask('pic', 'mask')
 
-    game.add.image(game.world.x_center, 320, bmd).anchor.set(0.5, 0);
+    game.add.image(game.world.x_center, 320, bmd).anchor.set(0.5, 0)
   end
 end
 
--- examples/sprites/dynamic-crop/main.rb
+++ examples/sprites/dynamic-crop/main.rb
@@ -3,30 +3,30 @@ require 'opal-phaser'
 
 class DynamicCrop < Phaser::State
   def preload
-    game.load.image('trsi', 'assets/pics/trsipic1_lazur.jpg');
+    game.load.image('trsi', 'assets/pics/trsipic1_lazur.jpg')
   end
 
   def create
-    @pic = game.add.sprite(0, 0, 'trsi');
+    @pic = game.add.sprite(0, 0, 'trsi')
     $pic = @pic
 
-    @w = @pic.width;
-    @h = @pic.height;
+    @w = @pic.width
+    @h = @pic.height
 
-    @rect = Phaser::Rectangle.new(0, 0, 128, 128);
+    @rect = Phaser::Rectangle.new(0, 0, 128, 128)
     $rect = @rect
 
-    @pic.crop(@rect);
+    @pic.crop(@rect)
   end
 
   def update
     if (game.input.x < @w && game.input.y < @h)
-      @pic.x = game.input.x;
-      @pic.y = game.input.y;
-      @rect.x = game.input.x;
-      @rect.y = game.input.y;
+      @pic.x = game.input.x
+      @pic.y = game.input.y
+      @rect.x = game.input.x
+      @rect.y = game.input.y
 
-      @pic.update_crop;
+      @pic.update_crop
     end
   end
 end
