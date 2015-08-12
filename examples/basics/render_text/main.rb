329dd3e0363a9b05a7064c99e23a76ccfbb05319
George D. Plymale II <georgedp@orbitalimpact.com>
2015-08-12 18:28:30 -0400

    fix screwup in last commit where it made a duplicate of each file's content

--- examples/basics/render_text/main.rb
+++ examples/basics/render_text/main.rb
@@ -34,39 +34,3 @@ class MainState < Phaser::State
 end
 
 Game.new
-require 'opal'
-require 'opal-phaser'
-
-class Text
-  def initialize(game)
-    @game = game
-  end
-  
-  def create
-    text  = "- phaser -\n with a sprinkle of \n pixi dust."
-    style = { font: "65px Arial", fill: "#ff0044", align: "center" }
-    
-    rendered_text = @game.add.text(@game.world.x_center - 300, 0, text, style)
-  end
-end
-
-class Game
-  def initialize
-    game  = Phaser::Game.new(width: 800, height: 600, renderer: Phaser::CANVAS, parent: "example")
-    state = MainState.new(game)
-    game.state.add(:main, state, true)
-  end
-end
-
-class MainState < Phaser::State
-  def initialize(game)
-    @game = game
-  end
-  
-  def create
-    @text = Text.new(game)
-    @text.create
-  end
-end
-
-Game.new
--- examples/bitmap_data/alpha_mask/main.rb
+++ examples/bitmap_data/alpha_mask/main.rb
@@ -33,38 +33,3 @@ class Game
 end
 
 Game.new
-require 'opal'
-require 'opal-phaser'
-
-class AlphaMask < Phaser::State
-  def preload
-    game.load.image('pic', 'assets/pics/questar.png')
-    game.load.image('mask', 'assets/pics/mask-test2.png')
-  end
-
-  def create
-    game.stage.background_color = 0x4d4d4d
-
-    game.add.text(64, 10, 'Source image', { font: '16px Arial', fill: '#ffffff' })
-    game.add.image(64, 32, 'pic')
-
-    game.add.text(400, 10, 'Alpha mask', { font: '16px Arial', fill: '#ffffff' })
-    game.add.image(400, 32, 'mask')
-
-    bmd = game.make.bitmap_data(320, 256)
-
-    bmd.alpha_mask('pic', 'mask')
-
-    game.add.image(game.world.x_center, 320, bmd).anchor.set(0.5, 0)
-  end
-end
-
-class Game
-  def initialize
-    game  = Phaser::Game.new(width: 800, height: 600, renderer: Phaser::AUTO, parent: "example")
-    state = AlphaMask.new(game)
-    game.state.add(:main, state, true)
-  end
-end
-
-Game.new
